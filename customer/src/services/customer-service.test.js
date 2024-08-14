a6e34f5b6e747a9ecab83dd796bb45567086406f
Sandeep Petkar <sandeeppetkar@Sandeeps-MacBook-Air.local>
2024-08-14 18:11:52 +0530

    added test case description

--- customer/src/services/customer-service.test.js
+++ customer/src/services/customer-service.test.js
@@ -1,5 +1,6 @@
 describe("CustomerService", () => {
   describe("SignIn", () => {
+    //valifating customer service user inputs
     test("validate user inputs", () => {});
 
     test("validate response", async () => {});
